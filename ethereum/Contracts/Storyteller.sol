8c1fa12e7d6ba9daa8ec1765de0d6f5804823286
Piotr <piotrryszewski@gmail.com>
2018-03-19 11:02:01 +0100

    Added modifiers and contructor for story struct

--- ethereum/Contracts/Storyteller.sol
+++ ethereum/Contracts/Storyteller.sol
@@ -12,10 +12,20 @@ contract Storyteller {
 
     Story[] public stories;
     address public administrator;
+    address public moderator;
+    address[] public moderators;
     mapping(address => bool) public approvers;
 
-    modifier restricted(){
+    //MODIFIERS - USER ACCESS
+    //Administrator - the highest tier of access, can appoint and revoke moderators
+    modifier admin(){
         require(msg.sender == administrator);
+        _;
+    }
+    //Moderator - User with access to moderate stories, appointed only by administrator
+    modifier moderatorAcc(){
+        require(checkIfModerator(msg.sender));
+
         _;
     }
 
@@ -23,23 +33,34 @@ contract Storyteller {
         administrator = msg.sender;
     }
 
-    function contribute() payable public {
-        require(msg.value > minimumContribution);
-        approvers[msg.sender] = true;
-        approversCount++;
+    function checkIfModerator(address user) view public returns (bool){
+      for(uint i = 0; i<moderators.length; i++){
+        if(moderators[i] == user){
+          return true;
+        }else{
+          continue;
+        }
+        return false;
+      }
     }
 
-    function createStory(string tittle, string story, , uint value,address recipient)
-        public restricted {
-            /* require(approvers[msg.sender]); */
-            Request memory newRequest = Request({
-                description: description,
-                value: value,
-                recipient: recipient,
-                complete: false,
-                approvalCount: 0
+
+    function createStory(string tittle, string story, address author)
+        public {
+            /* STRUCT:
+            string tittle;
+            string story;
+            address author;
+            uint rating;
+            mapping(address => bool) raters;
+            */
+            Story memory newStory = Story({
+                tittle: tittle,
+                story: story,
+                author: author,
+                rating: 0
             });
-        requests.push(newRequest);
+        stories.push(newStory);
     }
 
 
