424882bc4b80d9f811480d85abd174d60a0da7b3
Piotr <piotrryszewski@gmail.com>
2018-03-23 16:23:36 +0100

    Added header menu to layout

--- .next/dist/components/layout.js
+++ .next/dist/components/layout.js
@@ -14,6 +14,10 @@ var _head = require("./head");
 
 var _head2 = _interopRequireDefault(_head);
 
+var _header = require("./header");
+
+var _header2 = _interopRequireDefault(_header);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _jsxFileName = "C:\\Users\\piotr.ryszewski\\Documents\\Projects\\Storyteller\\components\\layout.js";
@@ -22,13 +26,18 @@ exports.default = function (props) {
   return _react2.default.createElement(_semanticUiReact.Container, {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 7
+      lineNumber: 8
     }
   }, _react2.default.createElement(_head2.default, {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 8
+      lineNumber: 9
+    }
+  }), _react2.default.createElement(_header2.default, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
     }
   }), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXGxheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkNvbnRhaW5lciIsIkhlYWQiLCJwcm9wcyIsImNoaWxkcmVuIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7O0FBQ1AsQUFBUzs7QUFDVCxBQUFPLEFBQVUsQUFFakI7Ozs7Ozs7O2tCQUFlLGlCQUFTLEFBQ3RCO3lCQUNFLEFBQUM7O2dCQUFEO2tCQUFBLEFBQ0E7QUFEQTtBQUFBLEdBQUEsa0JBQ0EsQUFBQzs7Z0JBQUQ7a0JBREEsQUFDQSxBQUNHO0FBREg7QUFBQSxZQUZGLEFBQ0UsQUFFUyxBQUdaO0FBUEQiLCJmaWxlIjoibGF5b3V0LmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL3Bpb3RyLnJ5c3pld3NraS9Eb2N1bWVudHMvUHJvamVjdHMvU3Rvcnl0ZWxsZXIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHNcXGxheW91dC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsIkNvbnRhaW5lciIsIkhlYWQiLCJIZWFkZXIiLCJwcm9wcyIsImNoaWxkcmVuIl0sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQSxBQUFPOzs7O0FBQ1AsQUFBUzs7QUFDVCxBQUFPLEFBQVU7Ozs7QUFDakIsQUFBTyxBQUFZLEFBRW5COzs7Ozs7OztrQkFBZSxpQkFBUyxBQUN0Qjt5QkFDRSxBQUFDOztnQkFBRDtrQkFBQSxBQUNBO0FBREE7QUFBQSxHQUFBLGtCQUNBLEFBQUM7O2dCQUFEO2tCQURBLEFBQ0EsQUFDQTtBQURBO0FBQUEsc0JBQ0EsQUFBQzs7Z0JBQUQ7a0JBRkEsQUFFQSxBQUNHO0FBREg7QUFBQSxZQUhGLEFBQ0UsQUFHUyxBQUdaO0FBUkQiLCJmaWxlIjoibGF5b3V0LmpzIiwic291cmNlUm9vdCI6IkM6L1VzZXJzL3Bpb3RyLnJ5c3pld3NraS9Eb2N1bWVudHMvUHJvamVjdHMvU3Rvcnl0ZWxsZXIifQ==
--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
-import Head from "./head"
+import Head from "./head";
+import Header from "./header";
 
 export default props => {
   return(
     <Container>
     <Head/>
+    <Header/>
       {props.children}
     </Container>
   );
